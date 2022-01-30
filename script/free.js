73cb08a5df58909b273bea7488f65f97b9ea3bd0
hannahdue <56439564+hannahdue@users.noreply.github.com>
2022-01-30 18:27:25 +0100

    script button activity

--- script/free.js
+++ script/free.js
@@ -1,6 +1,36 @@
 "use strict";
 var FreeTheorie;
 (function (FreeTheorie) {
-    console.log("skript verknüpft");
+    console.log("script connected");
+    window.addEventListener("load", handleLoad);
+    let btnM1;
+    let btnM2;
+    let btnM3;
+    let btnM4;
+    let btnM5;
+    let btnKontakt;
+    function handleLoad() {
+        console.log("window loaded");
+        btnM1 = document.querySelector("#btnM1");
+        btnM1.addEventListener("click", handleButtonClick);
+        btnM2 = document.querySelector("#btnM2");
+        btnM2.addEventListener("click", handleButtonClick);
+        btnM3 = document.querySelector("#btnM3");
+        btnM3.addEventListener("click", handleButtonClick);
+        btnM4 = document.querySelector("#btnM4");
+        btnM4.addEventListener("click", handleButtonClick);
+        btnM5 = document.querySelector("#btnM5");
+        btnM5.addEventListener("click", handleButtonClick);
+        btnKontakt = document.querySelector("#btnKontakt");
+        btnKontakt.addEventListener("click", handleKontaktButtonClick);
+    }
+    function handleButtonClick() {
+        console.log("button clicked");
+        window.alert("Vielen Dank, deine Antworten wurden eingereicht. Dein:e Professor:in wird sich bald bei mit dem Feedback bei dir melden.");
+    }
+    function handleKontaktButtonClick() {
+        console.log("button clicked");
+        window.alert("Deine Anfrage wurde versendet. Wir werden uns bald bei dir melden.");
+    }
 })(FreeTheorie || (FreeTheorie = {}));
 //# sourceMappingURL=free.js.map
--- script/free.ts
+++ script/free.ts
@@ -1,5 +1,47 @@
 namespace FreeTheorie {
 
-console.log("skript verknüpft");
+console.log("script connected");
+
+window.addEventListener("load", handleLoad);
+
+let btnM1: HTMLButtonElement;
+let btnM2: HTMLButtonElement;
+let btnM3: HTMLButtonElement;
+let btnM4: HTMLButtonElement;
+let btnM5: HTMLButtonElement;
+let btnKontakt: HTMLButtonElement;
+
+function handleLoad(): void {
+    console.log("window loaded");
+
+    btnM1 = <HTMLButtonElement>document.querySelector("#btnM1");
+    btnM1.addEventListener("click", handleButtonClick);
+
+    btnM2 = <HTMLButtonElement>document.querySelector("#btnM2");
+    btnM2.addEventListener("click", handleButtonClick);
+
+    btnM3 = <HTMLButtonElement>document.querySelector("#btnM3");
+    btnM3.addEventListener("click", handleButtonClick);
+
+    btnM4 = <HTMLButtonElement>document.querySelector("#btnM4");
+    btnM4.addEventListener("click", handleButtonClick);
+
+    btnM5 = <HTMLButtonElement>document.querySelector("#btnM5");
+    btnM5.addEventListener("click", handleButtonClick);
+
+    btnKontakt = <HTMLButtonElement>document.querySelector("#btnKontakt");
+    btnKontakt.addEventListener("click", handleKontaktButtonClick);
+}
+
+function handleButtonClick(): void {
+    console.log("button clicked");
+    window.alert("Vielen Dank, deine Antworten wurden eingereicht. Dein:e Professor:in wird sich bald bei mit dem Feedback bei dir melden.");
+}
+
+function handleKontaktButtonClick(): void {
+    console.log("button clicked");
+    window.alert("Deine Anfrage wurde versendet. Wir werden uns bald bei dir melden.");
+}
+
 
 }
